b5fc32d35fde1b58b054cdbc366042b1bbf8a8a5
jaddagiri <jaddagiri454@gmail.com>
2025-01-07 00:35:41 +0530

    02-variables.sh

--- 02-variables.sh
+++ 02-variables.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-echo "Rahim: : Hi Robert"
-echo "Robert: : Hi Rahim"
-echo "Rahim: : How are you doing"
+PERSON1=Ramesh
+PERSON2=Suresh
+echo "PERSON1: : Hi PERSON2"
+echo "PERSON2: : Hi PERSON1"
+echo "PERSON1: : How are you doing"
